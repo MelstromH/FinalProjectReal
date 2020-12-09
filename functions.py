c86a2a2cf913cf3259ba04a4de860dd2a664566a
Michael Smith <42483787+msmith2777@users.noreply.github.com>
2020-12-08 18:43:44 -0600

    Added the flight chart
    Haha finally figured out browser github

--- functions.py
+++ functions.py
@@ -1,5 +1,13 @@
+import numpy as np
+
 def printFlightMap(flight, row, col):
     for x in col:
         for y in col:
             print(flight[x][y])
             print("\n")
+
+chart = np.array([['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O'],['O','O','O','O']])
+
+newchart = chart.reshape(12,4)
+
+print(newchart)
