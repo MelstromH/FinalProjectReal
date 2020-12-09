49d36e7dcbba1dbc462af33fc7f1cec4a99df8be
danaennash <36461588+danaennash@users.noreply.github.com>
2020-12-08 18:27:52 -0600

    update main.py to add total revenue function

--- MelTasks/main.py
+++ MelTasks/main.py
@@ -3,3 +3,11 @@ def printFlightMap(flight, row, col):
         for y in col:
             print(flight[x][y])
             print("\n")
+
+           
+            
+def getTotalRevenue(total, row, col):
+	for x in col: 
+        for y in col:			
+			print(total[x][y])
+            print("\n")
