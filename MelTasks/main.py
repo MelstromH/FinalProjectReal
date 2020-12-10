94ffa70977408c223b41bf874fec655ae903d408
danaennash <36461588+danaennash@users.noreply.github.com>
2020-12-09 18:03:09 -0600

    attempt to update but possibly implemented wrong

--- MelTasks/main.py
+++ MelTasks/main.py
@@ -3,11 +3,26 @@ def printFlightMap(flight, row, col):
         for y in col:
             print(flight[x][y])
             print("\n")
-
-           
+         
             
-def getTotalRevenue(total, row, col):
-	for x in col: 
-        	for y in col:			
-			print(total[x][y])
-            		print("\n")
+def getTotalRevenue(arr, n):
+	
+	totalCost = 0
+	
+	for x in arr:
+		totalCost = totalCost + x
+		print(totalCost)
+		
+		x = 0
+			while x < 3:
+			x += 1
+		
+		y = 0
+			while y < 12:
+			y += 1
+			
+		if cost_matrix == "X":
+			totalCost += 
+			
+			
+	print(total)
